43a697e0ab20b342bd42ce622f4eab706345091d
kaiyoshida0318 <kaiyoshida0318@gmail.com>
2026-08-20 14:37:01 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
    データ: data/products.json（GitHub Contents API で読み書き）
    ========================================================= */
 
-const VERSION   = "0.31.0";
+const VERSION   = "0.32.0";
 const DATA_PATH = "data/products.json";
 const LS_CFG    = "kata_cfg_v1";
 const LS_DATA   = "kata_data_v2";
